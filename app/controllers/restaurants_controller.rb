43a81c96e13b64594971aa9abb6c3dac76ea1d06
Jefferson Cunha <jwcunha@gmail.com>
2018-10-02 12:40:51 -0300

    add average score and the total of reviews to Restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -11,7 +11,14 @@ class RestaurantsController < ApplicationController
   # GET /restaurants/1.json
   def show
     @reviews = Review.where(restaurant_id: @restaurant.id)
+    if @reviews.blank?
+      @avg_rating = 0
+    else
+      @avg_rating = @reviews.average(:rating).round(2)
+    end
   end
+
+
 
   # GET /restaurants/new
   def new
