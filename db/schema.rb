a3054c77bd0523d01ca67046d46678775e9ad269
Jefferson Cunha <jwcunha@gmail.com>
2018-10-01 23:16:02 -0300

    Link reviews to restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_01_203112) do
+ActiveRecord::Schema.define(version: 2018_10_02_013511) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2018_10_01_203112) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "restaurant_id"
   end
 
   create_table "users", force: :cascade do |t|
