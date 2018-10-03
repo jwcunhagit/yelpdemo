8a8d34522455a571d58c511f136b4f276bab3e9a
Jefferson Cunha <jwcunha@gmail.com>
2018-10-03 08:20:07 -0300

    Add validations to Restaurant form

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,20 @@ class Restaurant < ApplicationRecord
 	mount_uploader :image, ImageUploader
 
 	has_many :reviews
+
+	validates :name, :address, :phone, :website, :image, presence: true
+	validates :phone, format: { 
+		with: /\A\(\d{3}\) \d{3}-\d{4}\z/,
+    	message: "must be in the format (99) 9999-6750" 
+    }
+
+    validates :website, format: { 
+		with: /\Ahttp?:\/\/.*\z/,
+    	message: "must start with http:// or https://" 
+    }
+
+    validates :address, format: {
+    	with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/,
+    	message: "must be in the format 350 Fifth Avenue, New York, NY 10118"
+    }
 end
